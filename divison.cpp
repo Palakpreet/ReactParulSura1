2620ce8e16475fb910bfb5e7a38f41c481964cb0
Parul2004 <parul1007.be21@chitkara.edu.in>
2022-05-14 22:02:29 -0700

    div.cpp commited

---
+++ divison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter the value of a\n";
+    cin >> a;
+
+    int b;
+    cout << "Enter the value of b\n";
+    cin >> b;
+
+    cout << "Divison is " << a / b << endl;
+    return 0;
+}
